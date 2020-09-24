fbe254fc0eef8eb5c4310755ada91f2bf087a597
maxime-vaillant <max.vaillant1@orange.fr>
2020-09-24 23:02:26 +0200

    header

---
+++ src/components/header/screenTitle.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const screenTitle = ({title}) => {
+  return(
+    <div  className="header">
+      <h1>{title}</h1>
+    </div>
+  )
+}
+
+export default screenTitle;
