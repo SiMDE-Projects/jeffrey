3f3a5e2f5b2b5ad3ee0a1ce8c70e210c402c1336
Cesar Richard <cesar.richard@weezevent.com>
2020-10-25 22:49:01 +0100

    eslint fix

--- src/pages/commande.js
+++ src/pages/commande.js
@@ -1,43 +1,41 @@
-import React, { useState, useContext } from "react";
-import { withNamespaces } from "react-i18next";
-import { Accordion } from "semantic-ui-react";
-import "./css/commande.css";
-import data from "@assets/data.json";
-import Header from "@components/header/header";
-import Bottom from "@components/bottom-tab-bar/bottom-tab-bar";
-import LeftItem from "@components/ressources/LeftItem";
-import RightItem from "@components/ressources/RightItem";
-import LeftContent from "@components/ressources/LeftContent";
-import RightContent from "@components/ressources/RightContent";
-import TotalContext from "../context/total-context";
+import React, { useState } from 'react';
+import { withNamespaces } from 'react-i18next';
+import { Accordion } from 'semantic-ui-react';
+import './css/commande.css';
+import data from '@assets/data.json';
+import Header from '@components/header/header';
+import Bottom from '@components/bottom-tab-bar/bottom-tab-bar';
+import LeftItem from '@components/ressources/LeftItem';
+import RightItem from '@components/ressources/RightItem';
+import LeftContent from '@components/ressources/LeftContent';
+import RightContent from '@components/ressources/RightContent';
+import TotalContext from '../context/total-context';
 
 const product_data = data.Product;
 
 function Commande({ t }) {
-  const [total, setTotal] = useState(0)
+  const [total, setTotal] = useState(0);
 
   function handleTotal() {
-    setTotal(total+1);
+    setTotal(total + 1);
   }
 
-  const rootPanels = product_data.map((item, i) =>
-    i % 2 === 0
-      ? {
-          key: item.id,
-          title: { content: <LeftItem title={item.title} />, icon: "" },
-          content: { content: <LeftContent produits={item.details} /> }
-        }
-      : {
-          key: item.id,
-          title: { content: <RightItem title={item.title} />, icon: "" },
-          content: { content: <RightContent produits={item.details} /> }
-        }
-  );
+  const rootPanels = product_data.map((item, i) => (i % 2 === 0
+    ? {
+      key: item.id,
+      title: { content: <LeftItem title={item.title} />, icon: '' },
+      content: { content: <LeftContent produits={item.details} /> },
+    }
+    : {
+      key: item.id,
+      title: { content: <RightItem title={item.title} />, icon: '' },
+      content: { content: <RightContent produits={item.details} /> },
+    }));
 
   return (
     <div className="Main_container">
       <TotalContext.Provider value={{ total, handleTotal }}>
-        <Header title={t("buy")} />
+        <Header title={t('buy')} />
         <div className="Body_container">
           <Accordion defaultActiveIndex={-1} panels={rootPanels} />
         </div>
