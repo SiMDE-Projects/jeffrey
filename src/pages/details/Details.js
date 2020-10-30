64ee793c5961653d58ae20d3e4755af5805a3871
maxime-vaillant <max.vaillant1@orange.fr>
2020-10-30 12:54:40 +0100

    Add header on details page

--- src/pages/details/Details.js
+++ src/pages/details/Details.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { Icon } from 'semantic-ui-react';
 import './details.css';
 import { withNamespaces } from 'react-i18next';
 
 function Details({ t }) {
+    let history = useHistory();
+
     return (
         <div className='mainContainerDetails'>
+          <div className='goBack'>
+            <Icon name="angle left" size="big" onClick={() => history.goBack()} />
+          </div>
           <div className='bodyContainerDetails'>
           </div>
         </div>
