6f6e8c82d3ec44f27cb5a686cf3a1e004637ca0f
maxime-vaillant <max.vaillant1@orange.fr>
2021-01-21 00:26:58 +0100

    Starting request

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import { Form } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import './accueil.css';
 
 export default function() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = () => {
+        if (username !== '' && password !== '') {
+            axios.post('https://jeffrey.aguyot.fr/auth/login', {
+                'username': `${username}`,
+                'password': `${password}`
+            })
+            .then((res) => {
+              console.log(res.data)
+            })
+            .catch((error) => {
+              console.error(error)
+            })
+        }
+    }
+
     return (
         <div className="mainContainerHome">
             <div className="homeBackground">
@@ -12,14 +31,10 @@ export default function() {
             <div className="homeTitle"></div>
             <div className="bodyContainerHome">
                 <Form>
-                    <Form.Input icon="user" iconPosition="left" label="Username - CAS" placeholder="Username" />
-                    <Form.Input icon="lock" iconPosition="left" label="Password" type="password" />
+                    <Form.Input icon="user" iconPosition="left" label="Username - CAS" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} required />
+                    <Form.Input icon="lock" iconPosition="left" label="Password" type="password" value={password} onChange={e => setPassword(e.target.value)} required />
+                    <input type='submit' className="login"  value="LOGIN" onClick={() => handleSubmit()}/>
                 </Form>
-                <Link to="/order">
-                    <div className="login">
-                        <h2>LOGIN</h2>
-                    </div>
-                </Link>
             </div>
         </div>
     );
