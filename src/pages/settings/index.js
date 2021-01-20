3b7b98eaa02c2209a39781853b3a53e4662360d4
maxime-vaillant <max.vaillant1@orange.fr>
2021-01-20 19:11:05 +0100

    Lint Fix

--- src/pages/settings/index.js
+++ src/pages/settings/index.js
@@ -1,22 +1,20 @@
-import React, { useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { Icon } from 'semantic-ui-react';
 import './settings.css';
-import { useTranslation } from 'react-i18next';
 
 export default function() {
-    const { t } = useTranslation();
     let history = useHistory();
 
-    return(
-      <div className="headerContainer">
-          <div className='goBack'>
-            <Icon name="angle left" size="big" onClick={() => history.goBack()} />
-          </div>
-          <div className="title">
-              <h1>MES INFOS</h1>
-          </div>
-          <hr />
-      </div>
+    return (
+        <div className="headerContainer">
+            <div className="goBack">
+                <Icon name="angle left" size="big" onClick={() => history.goBack()} />
+            </div>
+            <div className="title">
+                <h1>MES INFOS</h1>
+            </div>
+            <hr />
+        </div>
     );
 }
